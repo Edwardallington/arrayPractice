5235eacdfc8086de62f2a3790e75e1f786db5078
ALLINGTON <ea0911399@otc.edu>
2018-10-30 09:38:22 -0500

    Added file with examples

--- firstProject.py
+++ firstProject.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import random
+arrylist = ['sheep', 'dogs', 'hamsters']
+for i in range(3):
+    choice = input("What other animals should be added to the list? ")
+    arrylist.append(choice)
+# print(arrylist)
+for i in range(len(arrylist)):
+    print(arrylist[i])
+# number = random.randint(0,5)
+# print(arrylist[number])
+
+print(random.choice(arrylist))
